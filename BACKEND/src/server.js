c0315da89dbdb5e94eb09e936a740e55c5a15bcc
aaryyang <gupta.22311328@viit.ac.in>
2025-06-21 17:13:35 +0530

    casing fixed

--- BACKEND/src/server.js
+++ BACKEND/src/server.js
@@ -35,10 +35,10 @@ app.use(rateLimiter);
 app.use("/api/notes", notesRoutes);
 
 if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+    app.use(express.static(path.join(__dirname,"../FRONTEND/dist")));
 
     app.get("*",(req,res)=>{
-        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
+        res.sendFile(path.join(__dirname,"../FRONTEND","dist","index.html"));
     })
 }
 
